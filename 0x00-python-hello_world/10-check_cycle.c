82578499bc5433756f3220442c78f22d3c1f7de3
eimanalif <eiman.fadlelmowla@gmail.com>
2023-10-30 14:28:28 -0700

    check

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include"lists.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+/**
+ *check_cycle - to check code
+ *Description:to check code
+ *@list: pointer
+ *Return: 0 or 1
+ */
+
+
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fast = list;
+	listint_t *slow = list;
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+			if (slow = fast)
+				return (1);
+	}
+	return (0);
+}
