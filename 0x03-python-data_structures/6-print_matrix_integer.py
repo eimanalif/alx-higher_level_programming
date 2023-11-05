baa93ec061c28892454e08c607dc6f1eec5d9d09
eimanalif <eiman.fadlelmowla@gmail.com>
2023-11-05 00:56:37 -0700

    6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
+    for list1 in matrix:
+        if len(list1) == 0:
+            print()
+        for i in range(len(list1)):
+            print("{}".format(list1[i]),
+                  end="\n" if i is len(list1) -1 else " ")
