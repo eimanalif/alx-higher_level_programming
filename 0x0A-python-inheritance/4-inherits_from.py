9b39d2e38173f979f7c2d289484941d17a7d91fc
eimanalif <eiman.fadlelmowla@gmail.com>
2023-12-04 10:10:46 -0800

    5

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+a module that return true or false
+'''
+
+
+def inherits_from(obj, a_class):
+    '''
+    function that returns True if the object is an instance of
+    a class that inherited (directly or indirectly) from the
+    specified class ; otherwise False.
+    '''
+    return isinstance(obj, a_class)
