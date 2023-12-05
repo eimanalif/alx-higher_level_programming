0f06b0593255ae7782bba6d9a9760ed8a5d9b74b
eimanalif <eiman.fadlelmowla@gmail.com>
2023-12-05 11:38:11 -0800

    7

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-'''defining append_ write fuction'''
+"""defining append_ write fuction"""
 
 
 def write_file(filename="", text=""):
-    '''append filename with utf-8'''
-    with open(filename, "a", encoding="utf-8") as f:
+    '''append filename with utf-8
+     Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+    Returns:
+        The number of characters appended.
+    '''
+    with open(filename, 'a', encoding="utf-8") as f:
         return f.write(text)
