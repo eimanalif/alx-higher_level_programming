8ad9a30e3cdabdeba2b0d36803e907f27ddc9d92
eimanalif <eiman.fadlelmowla@gmail.com>
2023-12-05 06:39:35 -0800

    1

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,6 +3,6 @@
 
 
 def write_file(filename="", text=""):
-    '''read filename'''
-    with open(filename, "a", encoding='utf-8') as f:
+    '''read filename with utf-8'''
+    with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
