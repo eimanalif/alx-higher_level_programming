f722cbd42eeabb0760ef5e3e5bca8fd0c4a09685
eimanalif <eiman.fadlelmowla@gmail.com>
2023-12-05 11:03:30 -0800

    7

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""class student"""
+"""Defines a class Student."""
 
 
-class student:
-    """represnt of student"""
+class Student:
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
-        """intialies student
+        """Initialize a new Student.
         Args:
             first_name (str): The first name of the student.
             last_name (str): The last name of the student.
@@ -16,5 +17,5 @@ class student:
         self.age = age
 
     def to_json(self):
-        """returns a dictionary representation of a student instance"""
+        """Get a dictionary representation of the Student."""
         return self.
