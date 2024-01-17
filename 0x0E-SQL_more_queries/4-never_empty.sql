efe2ed55eb59dadafedb327685f3f163d8fc56df
eimanalif <eiman.fadlelmowla@gmail.com>
2024-01-17 10:42:28 -0800

    d

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,3 @@
 -- creates the table id_not_null on your MySQL server.
-CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+-- creates a table
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
