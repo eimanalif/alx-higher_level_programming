0508f0dd5d0ef28709da8a818343e730ae3c163b
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-02-29 21:47:46 +0000

    2

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,19 +9,28 @@ def find_peak(list_of_integers):
     Returns:
     - A peak element from the list.
     """
-    if not list_of_integers:
+
+    ` if list_of_integers is None or len(list_of_integers) == 0:
         return None
 
-    low = 0
-    high = len(list_of_integers) - 1
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
 
-    while low < high:
-        mid = (low + high) // 2
+    mid_idx = int(len(list_of_integers) / 2)
 
-        # Compare mid with its neighbors
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
-            high = mid
+    if mid_idx != len(list_of_integers) - 1:
+        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx] and\
+           list_of_integers[mid_idx + 1] < list_of_integers[mid_idx]:
+            return list_of_integers[mid_idx]
+    else:
+        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx]:
+            return list_of_integers[mid_idx]
         else:
-            low = mid + 1
+            return list_of_integers[mid_idx - 1]
 
-    return list_of_integers[low]
+    if list_of_integers[mid_idx - 1] > list_of_integers[mid_idx]:
+        a_list = list_of_integers[0:mid_idx]
+    else:
+        a_list = list_of_integers[mid_idx + 1:]
+
+    return find_peak(a_list)
