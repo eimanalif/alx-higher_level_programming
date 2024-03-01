01c2fdaa960a3291391a8552449e6ae824522c5e
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-03-01 21:07:35 +0000

    0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" script that takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8).
+"""
+
+import sys
+import urllib.request
+import urllib.error
+
+url = sys.argv[1]
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as error:
+        print("Error code: {}".format(error.code))
