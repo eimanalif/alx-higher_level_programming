f7695ccfadbd623bed4bca82778e9bc29965df4c
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-03-01 22:21:40 +0000

    0

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     reponse = requests.get(url)
     if response.status_code >= 400:
-        print('Error code: {}'.format(response.status_code))
+        print("Error code: {}".format(response.status_code))
     else:
         print(response.text)
